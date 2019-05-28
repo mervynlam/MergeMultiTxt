204314fdce8bde82a7609197de28b5e8e0798ff9
MervynLam <mervynlam@yeah.net>
2019-05-28 23:29:00 +0800

    Update App.java

--- src/com/mervynlam/mergeMultiTxt/App.java
+++ src/com/mervynlam/mergeMultiTxt/App.java
@@ -35,16 +35,6 @@ public class App {
 			//目标文件名，就是文件夹名
 			String fileName = dir.getName()+".txt";	
 			File novel = new File(fileName);
-			System.out.println("输出"+fileName);
-			//不存在就创建
-			if (!novel.exists()) {
-				try {
-					novel.createNewFile();
-				} catch (IOException e1) {
-					System.out.println(fileName+"文件创建失败");
-					e1.printStackTrace();
-				}
-			}
 			//当前目录下的所有.txt文件
 			File[] files = dir.listFiles(new FilenameFilter() {
 				@Override
@@ -52,42 +42,57 @@ public class App {
 					return name.endsWith(".txt");
 				}
 			});
-			//输出流
-			BufferedWriter bw = null;
-			try {
-				bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), encoding));
-			} catch (Exception e1) {
-				System.out.println("BufferedWriter实例化错误");
-				e1.printStackTrace();
-			}
-			//遍历文件夹下的txt
-			for(File file : files) {
-				try {
-					//输入流
-					BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file.getAbsoluteFile()), encoding));
-					String str = null;
-					while((str = br.readLine()) != null){
-						str = str.replaceAll("@无限好文，尽在晋江文学城", "");
-						bw.write(str);
-						bw.newLine();
+			if (files.length != 0) {
+				System.out.println("输出"+fileName);
+				//不存在就创建
+				if (!novel.exists()) {
+					try {
+						novel.createNewFile();
+					} catch (IOException e1) {
+						System.out.println(fileName+"文件创建失败");
+						e1.printStackTrace();
 					}
-					bw.newLine();
-					br.close();
-					System.out.println("输出--"+file.getName());
+				}
+				//输出流
+				BufferedWriter bw = null;
+				try {
+					bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), encoding));
+				} catch (Exception e1) {
+					System.out.println("BufferedWriter实例化错误");
+					e1.printStackTrace();
+				}
+				int chapterNum = 0;
+				//遍历文件夹下的txt
+				for(File file : files) {
+					try {
+						bw.write("第"+(++chapterNum)+"章   ");
+						//输入流
+						BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file.getAbsoluteFile()), encoding));
+						String str = null;
+						while((str = br.readLine()) != null){
+							str = str.replaceAll("@无限好文，尽在晋江文学城", "");
+							bw.write(str);
+							bw.newLine();
+						}
+						bw.newLine();
+						br.close();
+						System.out.println("输出--"+file.getName());
+					} catch(Exception e) {
+						System.out.println(file.getName()+"输出错误");
+						e.printStackTrace();
+					}
+				}
+				try {
+					bw.flush();
+					bw.close();
 				} catch(Exception e) {
-					System.out.println(file.getName()+"输出错误");
+					System.out.println("合成错误");
 					e.printStackTrace();
 				}
+				System.out.println("输出"+fileName+"完成");
+				System.out.println("--------------------------");
 			}
-			try {
-				bw.flush();
-				bw.close();
-			} catch(Exception e) {
-				System.out.println("合成错误");
-				e.printStackTrace();
-			}
-			System.out.println("输出"+fileName+"完成");
-			System.out.println("--------------------------");
 		}
+		System.out.println("合并结束");
 	}
 }
